a007bfc5e527c89585b8d2bfaa67255601be0838
eyluldurus <eyll.durus@gmail.com>
2024-11-20 22:08:25 +0300

    Add files via upload

---
+++ sayilaritoplama.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("bir sayı girin: ");
+        int sayi = int.Parse(Console.ReadLine());
+
+        int toplam = 0;
+        
+        for (int i = 1; i <= sayi; i++)
+        {
+            toplam += i;
+        }
+
+        Console.WriteLine("1'den " + sayi + "'ye kadar olan sayıların toplamı: " + toplam);
+    }
+}
+
